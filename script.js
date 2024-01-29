f8591d27ee5fa7252be28878fb64f02722795540
ajay kumar <141300910+ajay903099@users.noreply.github.com>
2024-01-30 01:10:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const rulesDisplay = document.querySelector(".rules");
 const crossBtn = document.querySelector(".cross");
 const keysArray = Array.from(keys);
 
-// Function to get scores from local storage
+
 function updateScoreDisplay() {
   const scoresJSON = localStorage.getItem("scores");
   const updatedScores = scoresJSON
@@ -53,10 +53,10 @@ const valueOfKey = (name) => {
 };
 
 const getRandomNumber = () => {
-  // Generate a random decimal number between 0 (inclusive) and 1 (exclusive)
+  
   const randomDecimal = Math.random();
 
-  // random number between 1,2 and 3
+  
   const randomNumber = Math.floor(randomDecimal * 3) + 1;
 
   return randomNumber;
@@ -77,25 +77,24 @@ const playRockPaperScissors = (userChoice, compChoice) => {
 };
 
 const updateScores = (result) => {
-  // Retrieve the current scores from local storage
+  
   const scoresJSON = localStorage.getItem("scores");
   const scores = scoresJSON ? JSON.parse(scoresJSON) : { user: 0, computer: 0 };
 
-  // Update the scores based on the result
   if (result === "user") {
     scores.user += 1;
   } else if (result === "comp") {
     scores.computer += 1;
   }
 
-  // Save the updated scores back to local storage
+
   localStorage.setItem("scores", JSON.stringify(scores));
 
   updateScoreDisplay();
 };
 
 const updateResultSides = (userChoice, compChoice) => {
-  // setting user icon
+
   if (userChoice === 1) {
     userRock.style.display = "flex";
     userPaper.style.display = "none";
@@ -110,7 +109,7 @@ const updateResultSides = (userChoice, compChoice) => {
     userScissor.style.display = "flex";
   }
 
-  // setting pc icon
+  
   if (compChoice === 1) {
     pcRock.style.display = "flex";
     pcPaper.style.display = "none";
@@ -127,7 +126,7 @@ const updateResultSides = (userChoice, compChoice) => {
 };
 
 const updateResultZone = (result, userChoice, compChoice) => {
-  // make playing screen invisible and result screen visible
+
   playingZone.style.display = "none";
   resultZone.style.display = "flex";
 
@@ -177,32 +176,32 @@ const updateResultZone = (result, userChoice, compChoice) => {
 
 const keyClickHander = (event) => {
   const target = event.target;
-  const parentDiv = target.closest(".item"); // Get the closest parent div with the class "item"
+  const parentDiv = target.closest(".item");
 
   if (parentDiv) {
-    const keyClicked = parentDiv.id; // Fetching the key clicked
+    const keyClicked = parentDiv.id;
     console.log("keyClicked:", keyClicked);
     const userChoice = valueOfKey(keyClicked);
     console.log("userChoice:", userChoice);
 
-    // call the random response from computer
+
     const compChoice = getRandomNumber();
     console.log("compChoice:", compChoice);
 
-    // compare the results and genarate result
+    
     const result = playRockPaperScissors(userChoice, compChoice);
     console.log("Result: ", result);
 
-    // update the local storage based on the result
+    
     updateScores(result);
 
-    // update the result zone based on the result
+    
     updateResultZone(result, userChoice, compChoice);
   }
 };
 
 const playAgainHandler = (event) => {
-  // make playing screen visible and result screen invisible
+ 
   playingZone.style.display = "flex";
   resultZone.style.display = "none";
   mainScreen.style.display = "block";
@@ -210,7 +209,7 @@ const playAgainHandler = (event) => {
 };
 
 const nextPageHandler = () => {
-  // make winner screen visible
+  
   mainScreen.style.display = "none";
   winnerScreen.style.display = "flex";
   nextBtn.style.display = "none";
@@ -218,13 +217,13 @@ const nextPageHandler = () => {
 
 const showRulesHandler = () => {
   console.log("inisde showRulesHandler ");
-  // add rules and cross
+
   crossBtn.style.display = "flex";
   rulesDisplay.style.display = "flex";
 };
 
 const removeRulesHandler = () => {
-  // remove rules and cross
+ 
   crossBtn.style.display = "none";
   rulesDisplay.style.display = "none";
 };
